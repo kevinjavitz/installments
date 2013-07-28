0d587e17eb11c862689f832cf9ba501c49db5ce3
vvitkouski <a.vitkouski@gmail.com>
2013-07-29 00:07:11 +0300

    Authorize net issue

---
+++ app/code/local/ParadoxLabs/AuthorizeNetCim/Model/Onepage.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Authorize.Net CIM - Core bug fix.
+ *
+ * Paradox Labs, Inc.
+ * http://www.paradoxlabs.com
+ * 717-431-3330
+ *
+ * Having a problem with the plugin?
+ * Not sure what something means?
+ * Need custom development?
+ * Give us a call!
+ *
+ * @category    ParadoxLabs
+ * @package     ParadoxLabs_AuthorizeNetCim
+ * @author      Ryan Hoerr <ryan@paradoxlabs.com>
+ */
+
+/**
+ * 'registering' checkout for nominal items has proved problematic.
+ * For whatever boneheaded reason, the customer ID is set to 'true',
+ * and the account is never actually created. What? Yeah.
+ *
+ * Maybe there's a straightforward explanation as to why, and why it
+ * wasn't working for nominal items, but I don't know what that is.
+ */
+
+class ParadoxLabs_AuthorizeNetCim_Model_Onepage extends Itweb_Installments_Model_Checkout_Type_Onepage
+{
+    /**
+     * Prepare quote for customer registration and customer order submit
+     *
+     * @return Mage_Checkout_Model_Type_Onepage
+     */
+    protected function _prepareNewCustomerQuote()
+    {
+        $quote      = $this->getQuote();
+        $billing    = $quote->getBillingAddress();
+        $shipping   = $quote->isVirtual() ? null : $quote->getShippingAddress();
+
+        //$customer = Mage::getModel('customer/customer');
+        $customer = $quote->getCustomer();
+        /* @var $customer Mage_Customer_Model_Customer */
+        $customerBilling = $billing->exportCustomerAddress();
+        $customer->addAddress($customerBilling);
+        $billing->setCustomerAddress($customerBilling);
+        $customerBilling->setIsDefaultBilling(true);
+        if ($shipping && !$shipping->getSameAsBilling()) {
+            $customerShipping = $shipping->exportCustomerAddress();
+            $customer->addAddress($customerShipping);
+            $shipping->setCustomerAddress($customerShipping);
+            $customerShipping->setIsDefaultShipping(true);
+        } else {
+            $customerBilling->setIsDefaultShipping(true);
+        }
+
+        Mage::helper('core')->copyFieldset('checkout_onepage_quote', 'to_customer', $quote, $customer);
+        $customer->setPassword($customer->decryptPassword($quote->getPasswordHash()));
+        $customer->setPasswordHash($customer->hashPassword($customer->getPassword()));
+/**
+ * The magic line changes
+ */
+        $customer->save();
+        $quote->setCustomer($customer);
+            // ->setCustomerId(true);
+/**
+ * End the magic line changes
+ */
+    }
+}
